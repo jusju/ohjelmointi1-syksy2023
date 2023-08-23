06696c2104d589c1f296f9eec713a8a041a355da
Juslin Jukka <jusju@haaga-helia.fi>
2023-08-23 10:10:57 +0300

    first commit

---
+++ ohjelmointi1/src/viikko1perusteet/Moro.java
@@ -0,0 +1,10 @@
+package viikko1perusteet;
+
+public class Moro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Moro");
+	}
+
+}
---
+++ ohjelmointi1/src/viikko1perusteet/TerveMaailma.java
@@ -0,0 +1,10 @@
+package viikko1perusteet;
+
+public class TerveMaailma {
+
+	public static void main(String[] args) {
+		// TULOSTETAAN NAYTOLLE ELI KONSOLIIN
+		System.out.println("Terve maailma!");
+	}
+
+}
